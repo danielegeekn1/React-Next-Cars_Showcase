9055c631366103e8c74d3ea9eeb58fdf8817f76c
Daniele Basile <daniele.basile.job@gmail.com>
2023-06-11 15:05:15 +0200

    retrieving cars data from cars api

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -20,7 +20,7 @@ const SearchManufacturer = ({
 			  );
 	return (
 		<div className="search-manufacturer">
-			<Combobox>
+			<Combobox value={manufacturer} onChange={setManufacturer}>
 				<div className="relative w-full">
 					<Combobox.Button className="absolute top-[14px]">
 						<Image src="/car-logo.svg" width={20} height={20} alt="btn_logo" />
--- types/index.ts
+++ types/index.ts
@@ -10,3 +10,17 @@ export interface searchManufacturerProps {
 	manufacturer: string;
 	setManufacturer: (manufacturer: string) => void;
 }
+export interface carProps {
+	city_mpg: number;
+	class: string;
+	combination_mpg: number;
+	cylinders: number;
+	displacements: number;
+	drive: string;
+	fuel_type: string;
+	highway_mpg: string;
+	make: string;
+	model: string;
+	transmission: string;
+	year: number;
+}
